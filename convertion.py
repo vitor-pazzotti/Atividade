ac6db3e48e4728859f374a3697a153f2f2b527d0
Vitor <vitor.santos@semantix.com.br>
2019-08-16 09:41:33 -0300

    Decimal number into Roman

---
+++ convertion.py
@@ -0,0 +1,28 @@
+from random import randint
+
+def transform_roman(num):
+    var = [
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4,
+        1
+        ]
+    symbol = [
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV",
+        "I"
+        ]
+    roman = ''
+    i = 0
+    while num > 0 and num <= 3000:
+        for j in range(num // var[i]):
+            roman += symbol[i]
+            num -= var[i]
+        i += 1
+
+    return roman
+for i in range(10):
+    num = randint(1,3000)
+    print(f'{num}\t', transform_roman(num))
+
